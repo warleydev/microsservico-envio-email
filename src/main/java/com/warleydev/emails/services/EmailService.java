9691ee464d4b160b8bb91cd6a499c1677b19e715
Warley Dev <warleycm7@gmail.com>
2023-06-23 17:37:11 -0300

    microsservico funcionando para envio de emailgit add .!

--- src/main/java/com/warleydev/emails/services/EmailService.java
+++ src/main/java/com/warleydev/emails/services/EmailService.java
@@ -1,9 +1,15 @@
 package com.warleydev.emails.services;
 
 import com.warleydev.emails.entities.Email;
+import com.warleydev.emails.enums.StatusEmail;
 import com.warleydev.emails.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
@@ -11,7 +17,26 @@ public class EmailService {
     @Autowired
     private EmailRepository emailRepository;
 
-    public void sendEmail(Email entity) {
+    @Autowired
+    private JavaMailSender emailSender;
 
+    public Email sendEmail(Email entity) {
+        entity.setSendDateEmail(LocalDateTime.now());
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(entity.getEmailFrom());
+            message.setTo(entity.getEmailTo());
+            message.setSubject(entity.getSubject());
+            message.setText(entity.getText());
+            emailSender.send(message);
+
+            entity.setStatusEmail(StatusEmail.SENT);
+        }
+        catch (MailException e){
+            entity.setStatusEmail(StatusEmail.ERROR);
+        }
+        finally {
+            return emailRepository.save(entity);
+        }
     }
 }
